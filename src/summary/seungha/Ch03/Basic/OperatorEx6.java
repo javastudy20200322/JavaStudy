914c0d0565cd14d620b12fb7aa2106f77d998ffe
develiberta <develiberta@gmail.com>
2020-04-05 20:28:55 +0900

    이클립스 상 오류 해결 및 파일 정리

--- src/summary/seungha/Ch03/Basic/OperatorEx6.java
+++ src/summary/seungha/Ch03/Basic/OperatorEx6.java
@@ -4,7 +4,8 @@ class OperatorEx6 {
 	public static void main(String[] args) {
 		byte a = 10;
 		byte b = 20;
-		byte c = a + b;		// 컴파일 에러 발생. 명시적 형변환 필요
+//		byte c = a + b;		// 컴파일 에러 발생. 명시적 형변환 필요
+		byte c = (byte)(a+b);
 		System.out.println(c);
 	}
 }
