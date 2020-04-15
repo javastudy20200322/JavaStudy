3568919cf4a75a3d89b3f62c54cf3bec158a4abc
develiberta <develiberta@gmail.com>
2020-04-15 17:57:40 +0900

    폴더링 개인실습 예제 정리하고 연습문제만 남김

--- src/summary/seungha/Ch03/Basic/OperatorEx18.java
+++
@@ -1,9 +0,0 @@
-package summary.seungha.Ch03.Basic;
-
-class OperatorEx18 {
-	public static void main(String args[]) {
-		double pi = 3.141592;
-		double shortPi = Math.round(pi*1000) / 1000.0;
-		System.out.println(shortPi);
-	}
-}
--- src/summary/seungha/Ch04/Basic/FlowEx14.java
+++
@@ -1,8 +0,0 @@
-package summary.seungha.Ch04.Basic;
-
-class FlowEx14 {
-	public static void main(String[] args) {
-		for (int i=1,j=10; i<=10; i++,j--)
-			System.out.printf("%d \t %d%n", i, j);
-	}
-}
