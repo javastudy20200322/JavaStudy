b2dc4e64c3fce46ed705e9ea40836d5b35846eb5
develiberta <develiberta@gmail.com>
2020-04-19 17:28:13 +0900

    변수이름 서로 바뀌어서 수정

--- src/practice/week04/seungha/Birthday.java
+++ src/practice/week04/seungha/Birthday.java
@@ -11,17 +11,16 @@ public class Birthday {
 		int count = scanner.nextInt();
 		Student[] arrStudent = new Student[count];
 		for  (int i=0; i<count; i++) {
-			Scanner scanner2  = new Scanner(System.in);	// 왜 스캐너를 새로 지정해야 에러가 뜨지 않는지 의문
-			String name = scanner2.next();
-			int day = scanner2.nextInt();
-			int month = scanner2.nextInt();
-			int year = scanner2.nextInt();
+			String name = scanner.next();
+			int day = scanner.nextInt();
+			int month = scanner.nextInt();
+			int year = scanner.nextInt();
 			arrStudent[i] = new Student(name, day, month, year);
 		}
 		
 		/* 출력부 */
-		System.out.println(Student.older(arrStudent).name);
 		System.out.println(Student.younger(arrStudent).name);
+		System.out.println(Student.older(arrStudent).name);
 	}
 }
 
@@ -41,31 +40,31 @@ class Student {
 		this.year = year;
 	}
 	
-	static Student older(Student[] arrStudent) {
-		Student older = arrStudent[0];
-		for (int i=1; i<arrStudent.length; i++) {
-			if (older.year < arrStudent[i].year) {
-				older = arrStudent[i];
-			} else if (older.year == arrStudent[i].year && older.month < arrStudent[i].month) {
-				older = arrStudent[i];
-			} else if (older.year == arrStudent[i].year && older.month == arrStudent[i].month && older.day < arrStudent[i].day) {
-				older = arrStudent[i];
-			}
-		}
- 		return older;
-	}
-	
 	static Student younger(Student[] arrStudent) {
 		Student younger = arrStudent[0];
 		for (int i=1; i<arrStudent.length; i++) {
-			if (younger.year > arrStudent[i].year) {
+			if (younger.year < arrStudent[i].year) {
 				younger = arrStudent[i];
-			} else if (younger.year == arrStudent[i].year && younger.month > arrStudent[i].month) {
+			} else if (younger.year == arrStudent[i].year && younger.month < arrStudent[i].month) {
 				younger = arrStudent[i];
-			} else if (younger.year == arrStudent[i].year && younger.month == arrStudent[i].month && younger.day > arrStudent[i].day) {
+			} else if (younger.year == arrStudent[i].year && younger.month == arrStudent[i].month && younger.day < arrStudent[i].day) {
 				younger = arrStudent[i];
 			}
 		}
  		return younger;
+	}
+	
+	static Student older(Student[] arrStudent) {
+		Student older = arrStudent[0];
+		for (int i=1; i<arrStudent.length; i++) {
+			if (older.year > arrStudent[i].year) {
+				older = arrStudent[i];
+			} else if (older.year == arrStudent[i].year && older.month > arrStudent[i].month) {
+				older = arrStudent[i];
+			} else if (older.year == arrStudent[i].year && older.month == arrStudent[i].month && older.day > arrStudent[i].day) {
+				older = arrStudent[i];
+			}
+		}
+ 		return older;
 	}
 }
