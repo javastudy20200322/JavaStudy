4e640dc482f955fba25fc3ebf05dabca68b18b03
JeonDoHyun <wjsehgus1014@naver.com>
2020-04-21 13:48:55 +0900

    05

---
+++ bin/summary/dohyun/chapter05/Ex/ArrayEx5.java
@@ -0,0 +1,21 @@
+package chapter05.Ex;
+
+public class ArrayEx5 {
+
+	public static void main(String[] args) {
+
+		int sum = 0;
+		float average = 0f;
+
+		int score[] = { 100, 88, 90, 100, 100 };
+		for (int i = 0; i < score.length; i++) {
+			sum += score[i];
+		}
+		average = sum / (float) score.length;
+
+		System.out.println("총점 :" + sum);
+		System.out.println("평균 :" + average);
+
+	}
+
+}
