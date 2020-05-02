ff3c61b7042bf8bc11089457643ccdf63ad46205
develiberta <develiberta@gmail.com>
2020-05-02 17:18:10 +0900

    no message

--- src/practice/week06/Stack.java
+++ src/practice/week06/Stack.java
@@ -3,7 +3,7 @@ package practice.week06;
 /** [날짜] 2020-05-10
  *  [참고] 명품 JAVA Programming
  *  [문제]
- *  1. 주어진 Stack 인터페이스를 상속받아 문자열을 저장하는 StringStack 클래스를 구현하시오.
+ *  1. 주어진 Stack 인터페이스를 상속받아 문자열을 Stack 자료구조로 저장하는 StringStack 클래스를 구현하시오.
  *  2. 다음 실행결과를 출력하도록 StackApp 클래스에 main() 메서드를 작성하시오.
  *  [실행결과]
  *  총 스택 저장 공간의 크기 입력 >> 3
