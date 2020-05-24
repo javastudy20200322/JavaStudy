9ecb6356ca072221ddd12ec40d598c57b16d7473
jinie <jinie@DESKTOP-N72U89T>
2020-05-24 14:04:19 +0900

    20200524_commit

---
+++ src/summary/hyunjin/practice/Chapter11_Collection/Exercise11_03_04.java
@@ -0,0 +1,23 @@
+package summary.hyunjin.practice.Chapter11_Collection;
+
+public class Exercise11_03_04 {
+/*
+	[11-3] 다음 중 ArrayList에서 제일 비용이 많이 드는 작업은? 단, 작업도중에	ArrayList의 크기 변경이 발생하지 않는다고 가정한다.
+	a. 첫 번째 요소 삭제
+	b. 마지막 요소 삭제
+	c. 마지막에 새로운 요소 추가
+	d. 중간에 새로운 요소 추가
+
+	답 : d, arrayList는 순차적으로 진행되기때문에 가운데 값을 구하기 어려움
+*/
+
+/*
+	[11-4] LinkedList클래스는 이름과 달리 실제로는 이중 원형 연결리스트(doubly circular linked list)로 구현되어 있다. 
+		   LinkedList인스턴스를 생성하고 11개의 요소를	추가했을 때, 이 11개의 요소 중 접근시간(access time)이 
+		       가장 오래 걸리는 요소는 몇 번째 요소인가?
+		   
+	답 : 가장 마지막? 이거 걍 똑같을거 같은데
+		   
+*/
+}
+
---
+++ src/summary/hyunjin/practice/Chapter11_Collection/Exercise11_13.java
@@ -0,0 +1,144 @@
+package summary.hyunjin.practice.Chapter11_Collection;
+import java.util.*;
+
+//[11-13] 다음 코드는 문제11-12를 발전시킨 것으로 각 Player3들의 점수를 계산하고, 점수가 제일 높은 사람을 출력하는 코드이다. 
+//TreeMap의 정렬기준을 점수가 제일 높은 사람부터 내림차순이 되도록 아래의 코드를 완성하시오. 단, 동점자 처리는 하지 않는다.
+public class Exercise11_13 {
+	public static void main(String args[]) throws Exception	{
+		SutdaDeck3 deck = new SutdaDeck3();
+		deck.shuffle();
+		Player3[] pArr = {
+			new Player3("타짜", deck.pick(), deck.pick()),
+			new Player3("고수", deck.pick(), deck.pick()),
+			new Player3("물주", deck.pick(), deck.pick()),
+			new Player3("중수", deck.pick(), deck.pick()),
+			new Player3("하수", deck.pick(), deck.pick())
+		};
+		
+		TreeMap rank = new TreeMap(new Comparator(){
+			public int compare(Object o1, Object o2) {
+			/*	(1) 알맞은 코드를 넣어 완성하시오.	 */
+				if(o1 instanceof Player3 && o2 instanceof Player3) {
+					Player3 p1 = (Player3)o1;
+					Player3 p2 = (Player3)o2;
+					
+					return p2.point-p1.point;
+				}else {
+					return -1;
+				}
+			}
+		});
+		
+		for(int i=0; i < pArr.length;i++) {
+			Player3 p = pArr[i];
+			rank.put(p, deck.getPoint(p));
+			System.out.println(p+" "+deck.getPoint(p));
+		}
+		
+		System.out.println();
+		System.out.println("1위는 "+rank.firstKey()+"입니다.");
+	}
+}
+
+class SutdaDeck3 {
+	final int CARD_NUM = 20;
+	SutdaCard3[] cards = new SutdaCard3[CARD_NUM];
+	int pos = 0; // 다음에 가져올 카드의 위치
+	
+	HashMap jokbo = new HashMap(); // 족보를 저장할 HashMap
+	SutdaDeck3() {
+		for(int i=0;i < cards.length;i++) {
+			int num = i%10+1;
+			boolean isKwang = i < 10 && (num==1 || num==3 || num==8);
+			cards[i] = new SutdaCard3(num,isKwang);
+		}
+		registerJokbo(); // 족보를 등록한다.
+	}
+	
+	void registerJokbo() {
+		jokbo.put("KK", 4000);
+		jokbo.put("1010",3100); jokbo.put("12", 2060);
+		jokbo.put("99", 3090); jokbo.put("21", 2060);
+		jokbo.put("88", 3080); jokbo.put("14", 2050);
+		jokbo.put("77", 3070); jokbo.put("41", 2050);
+		jokbo.put("66", 3060); jokbo.put("19", 2040);
+		jokbo.put("55", 3050); jokbo.put("91", 2040);
+		jokbo.put("44", 3040); jokbo.put("110", 2030);
+		jokbo.put("33", 3030); jokbo.put("101", 2030);
+		jokbo.put("22", 3020); jokbo.put("104", 2020);
+		jokbo.put("11", 3010); jokbo.put("410", 2020);
+		jokbo.put("46", 2010);
+		jokbo.put("64", 2010);
+	}
+	int getPoint(Player3 p) {
+		if(p==null) return 0;
+		SutdaCard3 c1 = p.c1;
+		SutdaCard3 c2 = p.c2;
+		Integer result = 0;
+		
+		if(c1.isKwang && c2.isKwang) {
+			result = (Integer)jokbo.get("KK");
+		} else {
+			result = (Integer)jokbo.get(""+c1.num+c2.num);
+			if(result==null) {
+				result = new Integer((c1.num + c2.num) % 10 + 1000);
+			}
+		}
+		p.point = result.intValue();
+		return result.intValue();
+	}
+	SutdaCard3 pick() throws Exception {
+		SutdaCard3 c = null;
+		if(0 <= pos && pos < CARD_NUM) {
+			c = cards[pos];
+			cards[pos++] = null;
+		} else {
+			throw new Exception("남아있는 카드가 없습니다.");
+		}
+		return c;
+	}
+	
+	void shuffle() {
+		for(int x=0; x < CARD_NUM * 2; x++) {
+			int i = (int)(Math.random() * CARD_NUM);
+			int j = (int)(Math.random() * CARD_NUM);
+			SutdaCard3 tmp = cards[i];
+			cards[i] = cards[j];
+			cards[j] = tmp;
+		}
+	}
+} // SutdaDeck3
+
+class Player3 {
+	String name;
+	SutdaCard3 c1;
+	SutdaCard3 c2;
+	int point;
+	
+	Player3(String name, SutdaCard3 c1, SutdaCard3 c2) {
+		this.name = name ;
+		this.c1 = c1 ;
+		this.c2 = c2 ;
+	}
+	
+	public String toString() {
+		return "["+name+"]"+ c1.toString() +","+ c2.toString();
+	}
+} // class Player3
+class SutdaCard3 {
+	int num;
+	boolean isKwang;
+
+	SutdaCard3() {
+		this(1, true);
+	}
+	
+	SutdaCard3(int num, boolean isKwang) {
+		this.num = num;
+		this.isKwang = isKwang;
+	}
+	
+	public String toString() {
+		return num + ( isKwang ? "K":"");
+	}
+}
